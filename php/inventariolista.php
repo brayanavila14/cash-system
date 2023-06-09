0350e2e3afc5303e9c55583bb67af8c64a5adf0c
brayanavila14 <brayandavidavilaorozco@gmail.com>
2023-06-09 00:30:45 -0500

    upload v1.0.0

--- php/inventariolista.php
+++ php/inventariolista.php
@@ -12,24 +12,46 @@
         <h2>Inventario</h2>
     </div>
     <div class="subencabezado">
-        <h1 class="arregloinicial">ID del producto</h1>
-        <h1 class="text1">Nombre del producto</h1>
-        <h1 class="text2">Precio actual</h1>
+        <h1 class="arregloinicial">Nombre del producto</h1>
+        <h1 class="texttittle">Precio actual</h1>
         <h1 class="arreglofinal">Cantidad disponible</h1> 
-        </div>
-    <div class="inventario">
-        <div id="idproduct">
-            <p>002</p>
-        </div>
-        <div id="nameproduct">
-             <p class="nombre">Producto B</p>
-        </div>
-        <div id="precioproduct">
-            <p class="precio">$15.00</p>
-        </div>
-        <div id="cantproduct">
-            <p class="cantidad">50</p>
-        </div>
     </div>
+        <?php
+        session_start();
+        include("base-de-datos.php");
+
+        $newnombre_empresa = str_replace(" ", "_", $_SESSION['empresa']);
+
+        // Consulta para obtener los registros de la tabla inventario
+        $consulta = "SELECT * FROM inventario_" . $newnombre_empresa;
+        $resultado = mysqli_query($conexion, $consulta);
+
+        // Verificar si se encontraron registros
+        if (mysqli_num_rows($resultado) > 0) {
+        // Iterar sobre los registros y mostrarlos
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+            $nombre = $fila['nombre_producto'];
+            $precio = $fila['precio_actual'];
+            $cantidad = $fila['cantidad_disponible'];
+
+            // Mostrar los datos en el HTML
+            echo '<div class="inventario">';
+            echo '<div id="nameproduct">';
+            echo '<p class="nombre">' . $nombre . '</p>';
+            echo '</div>';
+            echo '<div id="precioproduct">';
+            echo '<p class="precio">$' . $precio . '</p>';
+            echo '</div>';
+            echo '<div id="cantproduct">';
+            echo '<p class="cantidad">' . $cantidad . '</p>';
+            echo '</div>';
+            echo '</div>';
+        }
+        } else {
+        echo '<h1 class="mensaje-error">No hay productos en el inventario.</h1>';
+        }
+
+    mysqli_close($conexion);
+?>
 </body>
 </html>
